a472b8cb20d3408cfe439123ad3b6a1a8f2b970b
p0pxyr <p0pxyr@gmail.com>
2012-12-11 05:56:26 +0800

    vote

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,10 @@
 class Post <ActiveRecord::Base
   attr_accessible :url, :title
-  validates :title, presence: true
-  validates :title, length:  {minimum:2}
-  validates :url, presence: true
   has_many :comments
   has_many :votes
-  
+
+  def vote_count
+  	votes.where(direction: "up").count - votes.where(direction: "down").count
+    	
+    end  
 end
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,7 +1,3 @@
 class Vote <ActiveRecord::Base
-  def create
-    post = Post.find(params[:post_id])
-    post.votes.create(direction: params[:direction])
-    redirect_to :root
-  end
+  attr_accessible :direction
 end
